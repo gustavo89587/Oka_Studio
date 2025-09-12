df59e04bf194653b16dbab82e66c93ecd62cfb2e
gustavo89587 <161786778+gustavo89587@users.noreply.github.com>
2025-09-12 11:44:18 -0300

    fix: remover next-pwa do next.config para build no Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import createNextPWA from '@ducanh2912/next-pwa';
-
-const withPWA = createNextPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
-/** @type {import('next').NextConfig} */
-const baseConfig = {
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   experimental: { typedRoutes: true },
 };
-
-export default withPWA(baseConfig);
+export default nextConfig;
